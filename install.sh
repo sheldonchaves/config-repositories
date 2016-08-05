ee6030ee9d415aaaf23ff99f7eba28b9084b5d0c
Sheldon <sheldonchaves@gmail.com>
2016-08-05 17:01:18 -0300

    script test

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+BASEDIR="$(pwd -P)"
+echo "Current directory:" "$BASEDIR"
+
+echo "Adding repositories"
+while read repo; do
+
+	if [[ $repo == *"#"* ]]
+	then
+		cd $BASEDIR
+	  	dir="${repo//[$'\t\r\n# ']}"
+		mkdir -p $dir;
+		cd $dir;
+	else
+		git clone $repo;
+	fi
+
+done < repositories.txt
